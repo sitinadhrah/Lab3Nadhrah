2e6e5ec4771f6b33c800b13ebbf59bf08266f863
acer <nadhrahroslan228@gmail.com>
2017-09-25 10:03:17 +0800

    test

---
+++ app/src/main/java/com/example/acer/nadhrahlab1/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.acer.nadhrahlab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button displayButton;
+    private EditText etName;
+    private TextView tvMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        displayButton = (Button) findViewById(R.id.button);
+        etName = (EditText) findViewById(R.id.editText);
+        tvMessage = (TextView) findViewById(R.id.textView);
+
+
+    }
+
+    public void displayText(View view) {
+        String text = etName.getText().toString();
+
+        tvMessage.setText("Welcome " + text);
+
+        Toast toast = Toast.makeText(this, "WELCOME " + text, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    public void resetText(View view) {
+        String text = etName.getText().toString();
+
+        etName.getText().clear();
+
+        Toast toast = Toast.makeText(this, "NAME HAVE BEEN RESET ", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
